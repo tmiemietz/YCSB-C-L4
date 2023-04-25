a6d9e87ee01d91b7a529559616ed90db34580d4b
vilaureu <vilaureu@users.noreply.github.com>
2023-04-25 15:14:29 +0200

    print benchmark results repeatedly
    Change-Id: I2a6b4fea77e73d314ddec53cc9c1bbc00d00fca5

--- server/ycsbc-l4/ycsbc-l4.cc
+++ server/ycsbc-l4/ycsbc-l4.cc
@@ -13,6 +13,8 @@
 #include <sstream>
 #include <vector>
 #include <future>
+#include <chrono>
+#include <thread>
 
 #include "core/utils.h"
 #include "core/timer.h"
@@ -131,9 +133,14 @@ int main(const int argc, const char *argv[]) {
     sum += n.get();
   }
   double duration = timer.End();
-  cerr << "# Transaction throughput (KTPS)" << endl;
-  cerr << props["dbname"] << '\t' << file_name << '\t' << num_threads << '\t';
-  cerr << total_ops / duration / 1000 << endl;
+
+  // Print result repeatedly so we do not miss it over an unreliable console.
+  while(true) {
+    cerr << "# Transaction throughput (KTPS)" << endl;
+    cerr << props["dbname"] << '\t' << file_name << '\t' << num_threads << '\t';
+    cerr << total_ops / duration / 1000 << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+  }
 }
 
 string ParseCommandLine(int argc, const char *argv[], utils::Properties &props) {
