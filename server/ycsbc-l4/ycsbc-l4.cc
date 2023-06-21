ddb62200dfc3d832c7161d98639e05daaad4dd3d
Till Miemietz <till.miemietz@barkhauseninstitut.org>
2023-06-21 14:02:18 +0200

    Add mising return statement in main function of YCSB.

--- server/ycsbc-l4/ycsbc-l4.cc
+++ server/ycsbc-l4/ycsbc-l4.cc
@@ -155,6 +155,8 @@ int main(const int argc, const char *argv[]) {
   cerr << "# Transaction throughput (KTPS)" << endl;
   cerr << props["dbname"] << '\t' << file_name << '\t' << num_threads << '\t';
   cerr << total_ops / duration / 1000 << endl;
+
+  return 0;
 }
 
 string ParseCommandLine(int argc, const char *argv[], utils::Properties &props) {
