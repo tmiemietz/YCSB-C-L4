2a819dd60e717372d813c55ecc368e0c73136bf3
Till Miemietz <till.miemietz@barkhauseninstitut.org>
2023-07-11 21:30:07 +0200

    lock_stl database: Correct locking scheme.
    This commit fixes several sources of heap corruptions that were caused by a
    too sloppy locking scheme. The commit also removes one layer of abstraction in
    the lock_stl database stack.

--- server/ycsbc-l4/db/lock_stl_db.h
+++ server/ycsbc-l4/db/lock_stl_db.h
@@ -13,14 +13,15 @@
 
 #include <string>
 #include <vector>
-#include "lib/lock_stl_hashtable.h"
+#include <mutex>
+#include "lib/stl_hashtable.h"
 
 namespace ycsbc {
 
 class LockStlDB : public HashtableDB {
  public:
   LockStlDB() : HashtableDB(
-      new vmp::LockStlHashtable<HashtableDB::FieldHashtable *>) { }
+      new vmp::StlHashtable<HashtableDB::FieldHashtable *>) { }
 
   ~LockStlDB() {
     std::vector<KeyHashtable::KVPair> key_pairs = key_table_->Entries();
@@ -30,9 +31,41 @@ class LockStlDB : public HashtableDB {
     delete key_table_;
   }
 
+  int Read(void *ctx, const std::string &table, const std::string &key,
+           const std::vector<std::string> *fields,
+           std::vector<KVPair> &result) {
+    std::lock_guard<std::mutex> lock(lock_);
+    return HashtableDB::Read(ctx, table, key, fields, result);
+  }
+
+  int Scan(void *ctx, const std::string &table, const std::string &key,
+           int len, const std::vector<std::string> *fields,
+           std::vector<std::vector<KVPair>> &result) {
+    std::lock_guard<std::mutex> lock(lock_);
+    return HashtableDB::Scan(ctx, table, key, len, fields, result);
+  }
+
+  int Update(void *ctx, const std::string &table, const std::string &key,
+             std::vector<KVPair> &values) {
+    std::lock_guard<std::mutex> lock(lock_);
+    return HashtableDB::Update(ctx, table, key, values);
+  }
+
+  int Insert(void *ctx, const std::string &table, const std::string &key,
+             std::vector<KVPair> &values) {
+    std::lock_guard<std::mutex> lock(lock_);
+    return HashtableDB::Insert(ctx, table, key, values);
+  }
+
+  int Delete(void *ctx, const std::string &table,
+             const std::string &key) {
+    std::lock_guard<std::mutex> lock(lock_);
+    return HashtableDB::Delete(ctx, table, key);
+  }
+
  protected:
   HashtableDB::FieldHashtable *NewFieldHashtable() {
-    return new vmp::LockStlHashtable<const char *>;
+    return new vmp::StlHashtable<const char *>;
   }
 
   void DeleteFieldHashtable(HashtableDB::FieldHashtable *table) {
@@ -52,6 +85,9 @@ class LockStlDB : public HashtableDB {
   void DeleteString(const char *str) {
     delete[] str;
   }
+
+ private:
+  mutable std::mutex lock_;
 };
 
 } // ycsbc
